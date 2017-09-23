812a0da51c0783dd2d6a9fadcedc30f707ad847c
Alex Olson <alexkellenolson@gmail.com>
2017-09-23 11:26:08 +0200

    scanner and location watcher pretty much done.

--- scanner.js
+++ scanner.js
@@ -3,16 +3,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const request = require('request');
 const Estimote = require('bleacon').Estimote;
 
-function startScanning() {
-  Estimote.discoverAll((estimote) => {
-    // console.log(estimote.uuid);
-  });
-}
-
-
 module.exports = () => {
+
+  function startScanning() {
+    Estimote.discoverAll((estimote) => {
+      const { uuid } = estimote;
+      Location.find({}).sort('-createdAt').exec((err, docs) => {
+        const { lat, lng } = docs[0];
+        const date = Date.now();
+        request({
+          method: 'post',
+          uri: 'http://localhost:3000:/pings',
+          json: {
+            uuid,
+            lat,
+            lng,
+            date,
+          },
+        }, (err, res, body) => {
+          if (err) {
+            console.log('/pings API error');
+            console.log(err);
+            return;
+          }
+          if (res.statusCode !== 200) {
+            console.log('/pings API error');
+            console.log(res.statusCode);
+            return;
+          }
+        });
+      });
+
+    });
+  }
+
   startScanning();
 
   mongoose.connect('mongodb://localhost/hbs-scanner')
